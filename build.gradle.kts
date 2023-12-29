bbab36f96744eb049a3cab5cd68c6ecd8f97dce7
Kishan Sambhi <kishansambhi@hotmail.co.uk>
2023-12-29 01:21:37 +0000

    Feat: Move to pycharm

--- build.gradle.kts
+++ build.gradle.kts
@@ -43,6 +43,7 @@ intellij {
 
     // Plugin Dependencies. Uses `platformPlugins` property from the gradle.properties file.
     plugins = properties("platformPlugins").map { it.split(',').map(String::trim).filter(String::isNotEmpty) }
+    downloadSources = false
 }
 
 // Configure Gradle Changelog Plugin - read more: https://github.com/JetBrains/gradle-changelog-plugin
